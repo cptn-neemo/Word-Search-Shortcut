7ea5cfb444f7ed695623de6502c47af46013e4a5
Max <maxhuddleston@gmail.com>
2017-06-27 17:23:54 -0500

    Third commit

--- main.py
+++ main.py
@@ -40,16 +40,47 @@ def left_horizontal_check(word, board):
     for row in new_board:
         board.append(row[::-1])
     return board
-#function for doing all the checks on the board
-def all_check(word, board):
-    board = right_horizontal_check(word, board)
-    board = left_horizontal_check(word, board)
+
+def up_down_check(word,board):
+    new_board = []
+    each_col = []
+    count = 0
+    len_row = len(board[0])
+    for col in range(len_row):
+        for i in range(len(board)):
+            each_col.append(board[i][col])
+        new_board.append(each_col)
+        each_col = []
+
+    horizontal_check(word,new_board)
+
+    len_row = len(new_board[0])
+
+    board = []
+    each_col = []
+    for col in range(len_row):
+        for i in range(len(new_board)):
+            each_col.append(new_board[i][col])
+        board.append(each_col)
+        each_col = []
+
+
     return board
 
 
-board = [['m', 'o', 'n', 'k', 'e'], ['m', 'k', 'n', 'o', 'm'], ['a', 'b', 'c', 'd', 'd']]
-words = 'monk'
-print('Original board: '),
 
+def all_check(word, board):
+    board = right_horizontal_check(word, board)
+    board = left_horizontal_check(word, board)
+    board = up_down_check(word,board)
+    return board
+
+
+board = [['m', 'o', 'n', 'k', 'e'], ['a', 'k', 'n', 'o', 'm'], ['x', 'b', 'c', 'd', 'd']]
+words = 'max'
+print('Original board: '),
+print(range(len(board)))
 board = all_check(words, board)
 print('new board: '), print(board)
+
+
