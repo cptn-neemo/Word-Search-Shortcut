e83a8f2f6e6ab98a2d3bc36749fded732791fc38
Max <maxhuddleston@gmail.com>
2017-07-10 19:42:07 -0500

    Final Version

--- README.txt
+++ README.txt
@@ -1,3 +1,9 @@
 A python script for finding the words in a word find excercise.
 
+Instructions:
+    1. Create a new text file in the same directory as the script
+    2. Type in the board from the word finder, with a space between each letter and no extra spaces on the end
+    3. Run the script, and enter the words you want found
+    4. The script will create a new text file called 'Final_Output.txt'. This will contain the solutions in uppercase
+
 NOTE: Still figuring out the commit system on git, that's why most of the commit messages don't make sense.
--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-def horizontal_check(word_1, board_1):
+#Function definitions
+
+#Main left to right horizontal check
+def horizontal_check(word, board):
     # index of place on the board
     count = 0
-    # index of place in the word
-    place = 0
-
-    # check right
-
     # Go through each row in the board, then each index of the row. if the start of the word is that letter, then
     # check if each letter to the right is in the board
-    for col in board_1:
-        for p in range(len(col)):
+    for row in board:
+        for p in range(len(row)):
+            #Index of place in the word
             place = 0
-            if word_1[place] == col[p] or word_1[place] == col[p].lower():
+            if word[place] == row[p] or word[place].lower() == row[p].lower():
                 count = p
-                for pl in word_1:
+                for pl in word:
+                    #Check to see if going off of the board
                     try:
-                        if pl == col[count]:
+                        if pl == row[count] or pl.lower() == row[count].lower():
                             count += 1
                         else:
                             break
                     except:
                         break
+                #Make the word uppercase
                 else:
                     count = p
                     place = 0
-                    for i in range(len(word_1)):
-                        col[count] = word_1[place].upper()
+                    for i in range(len(word)):
+                        row[count] = word[place].upper()
                         count += 1
                         place += 1
                     return board
+
+#Check from left to right
 def right_horizontal_check(word, board):
     horizontal_check(word, board)
     return board
-#Reverse the board and then do the horizontal check
+
+#Reverse the board from right to left and then do the horizontal check
 def left_horizontal_check(word, board):
     new_board = [row[::-1] for row in board]
     horizontal_check(word, new_board)
@@ -41,6 +45,7 @@ def left_horizontal_check(word, board):
         board.append(row[::-1])
     return board
 
+#Flip the board to apply the horizontal check method
 def up_down_reverse(board,new_board):
     new_board = []
     each_col = []
@@ -52,6 +57,8 @@ def up_down_reverse(board,new_board):
         new_board.append(each_col)
         each_col = []
     return new_board
+
+#Up to down vertical check
 def up_down_check(word,board):
     new_board = []
     new_board = up_down_reverse(board,new_board)
@@ -62,6 +69,7 @@ def up_down_check(word,board):
     board = up_down_reverse(new_board,board)
     return board
 
+#Down to up vertical check
 def down_up_check(word,board):
     new_board = []
     #Switch the orientation
@@ -80,7 +88,6 @@ def down_up_check(word,board):
 
     return board
 
-
 #The main check diagonal function: Top right -> Bottom left
 def diagonal_check(word,board):
 
@@ -91,7 +98,7 @@ def diagonal_check(word,board):
     for row in range(len(board)):
         for col in range(len(board[row])):
             word_place = 0
-            if word[word_place] == board[row][col]:
+            if word[word_place] == board[row][col] or word[word_place].lower() == board[row][col].lower():
                 r = row
                 c = col
 
@@ -132,7 +139,6 @@ def brTL_diagonal_check(word,board):
 
     return board
 
-
 #Diagonal check: Top left -> Bottom right
 def trBL_diagonal_check(word,board):
     new_board = board
@@ -147,6 +153,7 @@ def trBL_diagonal_check(word,board):
 
     return board
 
+#Bottom left -> Top right check
 def blTR_diagonal_check(word,board):
     new_board = board[::-1]
 
@@ -156,7 +163,7 @@ def blTR_diagonal_check(word,board):
 
     return board
 
-
+#Do all of the checks and return the board
 def all_check(word, board):
     board = right_horizontal_check(word, board)
     board = left_horizontal_check(word, board)
@@ -168,31 +175,42 @@ def all_check(word, board):
     board = blTR_diagonal_check(word,board)
     return board
 
-"""
-board = [['m', 'd', 'g', 'k', 'a'], ['a', 'o', 'o', 'o', 'm'], ['d', 'b', 'g', 'a', 'm']]
-words = 'dog'
-print('Original board: '), print(board)
-board = all_check(words, board)
-print('new board: '), print(board)
-print()
-"""
+#--------------------------------------------------------------------------
+#Main
 
-#Create a list of lists with the board
+#Create a list of lists for the board
 board = []
-with open('Sample_Input.txt') as o:
-    for line in o:
+
+text_file = input('Please enter the name of the text file: ')
+
+#Append the lines of the text file to the board
+with open(text_file) as txt:
+    for line in txt:
         board.append(line.rstrip().split(' '))
 
+#List for the words
+words = []
 
+#Get all of the words
+while(True):
+    dec = input('Enter 1 to add a word, enter 2 to find the words: ')
+    if dec == '1':
+        w = input('Please enter the word: ')
+        w.lower()
+        words.append(w)
+        print()
+    else:
+        break
 
-words = ['dog','cat','max','dad','hello','good','bad','true']
+#Create the final text file
+txt = open('Final_Output.txt','w+')
+
 
 for word in words:
     board = all_check(word,board)
 
+#Write the new board to the text file
 for row in board:
-    print(row)
-
-
-
-
+    for let in row:
+        txt.write(let + ' ')
+    txt.write('\n')
